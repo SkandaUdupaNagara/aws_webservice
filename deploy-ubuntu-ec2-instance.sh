c62a4ba88637a8c4247926f3d3b2897719f81d86
SkandaUdupaNagara <skandanagara@gmail.com>
2023-03-24 13:18:30 +0000

    install Nginx web server and start the service
    Nginx server install and start service, .pem file permission changed to read only

--- deploy-ubuntu-ec2-instance.sh
+++ deploy-ubuntu-ec2-instance.sh
@@ -12,6 +12,8 @@ aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protoco
 # Create a new key pair
 export KEY_NAME=mykeypair
 aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > $KEY_NAME.pem
+# Change permission of file mykeypair.pem to read only and no access to other users
+chmod 400 $KEY_NAME.pem
 
 # Launch a new instance with ports 80 and 22 open
 aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --key-name $KEY_NAME --security-group-ids $SECURITY_GROUP_ID
@@ -20,4 +22,8 @@ aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --ke
 export INSTANCE_ID=$(aws ec2 describe-instances --query 'Instances[0].InstanceId' --output text)
 export PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
+# Install Nginx web server and start the service
+ssh -i $KEY_NAME.pem ubuntu@$PUBLIC_IP 'sudo apt-get update && sudo apt-get install -y nginx && sudo service nginx start'
+
 echo "Instance launched with ID $INSTANCE_ID and public IP address $PUBLIC_IP"
+
